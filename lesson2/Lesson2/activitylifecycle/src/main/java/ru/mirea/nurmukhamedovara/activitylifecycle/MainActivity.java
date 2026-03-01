48be1c8ef8de305f4c5bb708cd7530a0ae39ed86
Русмина Нурмухамедова <rusmina060606@gmail.com>
2026-03-02 00:58:56 +0300

    Add lesson2

---
+++ lesson2/Lesson2/activitylifecycle/src/main/java/ru/mirea/nurmukhamedovara/activitylifecycle/MainActivity.java
@@ -0,0 +1,79 @@
+package ru.mirea.nurmukhamedovara.activitylifecycle;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "LifecycleTag";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate()"); // Log.i - информационное сообщение
+
+        // Проверка, не пустой ли Bundle (восстановление после поворота)
+        if (savedInstanceState != null) {
+            Log.d(TAG, "onCreate() — Восстанавливаем состояние");
+        } else {
+            Log.d(TAG, "onCreate() — Первый запуск");
+        }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart()");
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState()");
+        if (savedInstanceState != null) {
+            // Здесь можно восстановить текст
+            Log.d(TAG, "Восстанавливаем текст из Bundle");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState()");
+        // Здесь можно сохранить текст из EditText
+        outState.putString("key_text", "Какое-то значение");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart()");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy()");
+    }
+
+}
